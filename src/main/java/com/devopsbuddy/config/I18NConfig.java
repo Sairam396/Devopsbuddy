f9e838e489c675508391301dce3aa5c0b42b2284
Sairam396 <sairamchowdary01@gmail.com>
2017-01-16 15:36:19 -0800

    Added i18n capability to the web skeleton

---
+++ src/main/java/com/devopsbuddy/config/I18NConfig.java
@@ -0,0 +1,21 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Created by rjeshg on 1/16/17.
+ */
+@Configuration
+public class I18NConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("classpath:i18n/messages");
+        // checks for new messages for every 30 mintues
+        resourceBundleMessageSource.setCacheSeconds(1800);
+        return resourceBundleMessageSource;
+    }
+}
