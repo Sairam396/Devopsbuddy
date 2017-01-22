02dafb1a3c004ed06a757d9b6e6b30f1d7e9ac9c
Sairam396 <sairamchowdary01@gmail.com>
2017-01-21 17:14:18 -0800

    Added unique indexes for username and email in the user entity

--- src/test/java/com/devopsbuddy/test/integration/RepositoriesIntegrationTest.java
+++ src/test/java/com/devopsbuddy/test/integration/RepositoriesIntegrationTest.java
@@ -13,7 +13,9 @@ import com.devopsbuddy.enums.RolesEnum;
 import com.devopsbuddy.utils.UserUtils;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -37,6 +39,8 @@ public class RepositoriesIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Rule public TestName testName = new TestName();
 
     @Before()
     public void init() {
@@ -68,8 +72,11 @@ public class RepositoriesIntegrationTest {
     @Test
     public void createNewUser() throws Exception {
 
+        String username = testName.getMethodName();
+        String email = testName.getMethodName() + "@devopsbuddy.com";
 
-        User basicUser = createUser();
+
+        User basicUser = createUser(username, email);
 
 
         User newlyCreatedUser = userRepository.findOne(basicUser.getId());
@@ -89,7 +96,10 @@ public class RepositoriesIntegrationTest {
     @Test
     public void testDeleteUser() throws  Exception {
 
-        User basicUser = createUser();
+        String username = testName.getMethodName();
+        String email = testName.getMethodName() + "@devopsbuddy.com";
+
+        User basicUser = createUser(username, email);
         userRepository.delete(basicUser.getId());
     }
 
@@ -103,12 +113,12 @@ public class RepositoriesIntegrationTest {
 
     }
 
-    private User createUser() {
+    private User createUser(String username, String email) {
         Plan basicPlan = createPlan(PlansEnum.BASIC);
         planRepository.save(basicPlan);
 
 
-        User basicUser = UserUtils.createBasicUser();
+        User basicUser = UserUtils.createBasicUser(username, email);
         basicUser.setPlan(basicPlan);
 
         Role basicRole = createRole(RolesEnum.BASIC);
